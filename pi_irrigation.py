6e205d2a7e8a6c6da35c3b757cefbd7c0a2ff40a
Ciro Anacleto <ciroanacleto@gmail.com>
2017-11-25 21:11:48 -0200

    Add logging

--- pi_irrigation.py
+++ pi_irrigation.py
@@ -1,36 +1,48 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 
 import time
 import schedule
 import RPi.GPIO as GPIO
+import logging
+
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(format= LOG_FORMAT,
+                    filename='./pi_irrigation.log',
+                    level=logging.INFO)
+log = logging.getLogger()
 
 relay_pin = 12
 first_hour = "10:00"
 second_hour = "22:00"
-watering_time = 600 #10 min
+watering_time = 360 #6 min
 
 def setup():
+    log.info('Config pins')
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(relay_pin, GPIO.OUT)
     GPIO.output(relay_pin, GPIO.HIGH)
 
 def irrigate():
     GPIO.output(relay_pin, GPIO.LOW)
-    print("Solenoid is on")
+    log.info('Solenoid is on')
 
     time.sleep(watering_time)
 
     GPIO.output(relay_pin, GPIO.HIGH)
-    print("Solenoid is off")
+    log.info('Solenoid is off')
 
 def destroy():
     GPIO.output(relay_pin, GPIO.HIGH)
-    print("Destroying")
+    log.info('Cleanning up pins')
     GPIO.cleanup()
 
 if __name__ == '__main__':
     setup()
+
+    log.info('Setting first irrigation time to ' + first_hour)
     schedule.every().day.at(first_hour).do(irrigate)
+
+    log.info('Setting second irrigation time to ' + second_hour)
     schedule.every().day.at(second_hour).do(irrigate)
     try:
         while True:
